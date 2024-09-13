96d2c070246b892a0a979199d6657cdbc42ddc3a
Mark <cabilesmp@gmail.com>
2024-09-13 16:36:18 +0800

    Initial proof of concept

--- js/homepage.js
+++ js/homepage.js
@@ -1,8 +1,31 @@
-const navLinks = document.querySelectorAll('.navLink')
+const navLinks = document.querySelectorAll('.nav-link')
+const detailsBtn = document.getElementById('detailsBtn')
+const sections = document.querySelectorAll('section')
 
 navLinks.forEach((n) => {
     n.addEventListener('click', () => {
         navLinks.forEach((n1) => n1.classList.remove('active'))
         n.classList.add('active')
+    })
+})
+
+detailsBtn.addEventListener('click', () => {
+    document.getElementById('location').scrollIntoView({behavior: 'smooth'})
+})
+
+document.addEventListener('scroll', () => {
+    sections.forEach((s) => {
+        const sectionTop = s.offsetTop
+        const sectionHeight = s.offsetHeight
+
+        if (scrollY >= sectionTop - 300 && scrollY < sectionTop + sectionHeight) {
+            let hrefValue = '#' + s.getAttribute('id')
+            navLinks.forEach((n) => {
+                n.classList.remove('active')
+                if (n.getAttribute('href') === hrefValue) {
+                    n.classList.add('active')
+                }
+            })
+        }
     })
 })
